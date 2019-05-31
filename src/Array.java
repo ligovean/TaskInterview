5278ca393ce0a9ba8252e7245dd9a94bae1c784d
ligovean <ligomail@rambler.ru>
2019-06-01 00:27:47 +0300

    Задание для собеседования !!!Корректно работает для нечетных размерностей массива

--- src/Array.java
+++ src/Array.java
@@ -1,3 +1,71 @@
 public class Array{
+    private int n ;
+    private int[][] arr;
+
+    public Array(int n) {
+        this.n = n;
+        this.arr= new int[n][n];
+        fillLine2(n,1,new int[]{0,0});
+    }
+
+    public void printArr(){
+        StringBuffer res;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(String.format("%02d", arr[i][j])+" ");
+            }
+            System.out.printf("\n");
+        }
+    }
+
+    //РЕКУРСИВНЫЙ МЕТОД ЗАПОЛНЕНИЯ СПИРАЛИ
+    public int[] fillLine2(int demn,int loop,int[] res){
+        //int[] res = {1,1};
+        int yLine = res[0];
+        int xLine = res[1];
+        int lastN;
+        if (loop!=1)
+            lastN = arr[yLine][xLine-1]+1;
+        else
+            lastN = 1;
+
+        //Заполнение последнего элемента
+        if (lastN>=demn*demn-1)
+        {arr[yLine][xLine] = lastN;
+            res[0]=yLine;
+            res[1]=xLine;
+            return res;}
+
+        for (int tern=1;tern<5;tern++){
+            for (int i=0; i<demn-((loop-1)*2+1); i++){
+                switch (tern) {
+                    case 1:
+                        yLine = loop-1;
+                        xLine = i+loop-1;
+                        arr[yLine][xLine] = lastN;//[Y][X]
+                        break;
+                    case 2:
+                        yLine = i+loop-1;
+                        xLine = demn-loop;
+                        arr[yLine][xLine] = lastN;//[Y][X]
+                        break;
+                    case 3:
+                        yLine = demn-loop;
+                        xLine = demn-loop-i;
+                        arr[yLine][xLine] = lastN;//[Y][X]
+                        break;
+                    case 4:
+                        yLine = demn-loop-i;
+                        xLine = loop-1;
+                        arr[yLine][xLine] = lastN;//[Y][X]
+                        break;
+                }
+                lastN++;
+            }
+        }
+        res[0]=yLine;
+        res[1]=xLine+1;
+        return fillLine2(demn,loop+1,res);
+    }
 
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,7 @@
 public class Main2 {
 
     public static void main(String[] args) {
-
+        Array arr1 = new Array(5);
+        arr1.printArr();
     }
 }
