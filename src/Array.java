76ed33ab4a1e9a0432365cc6664a7e1dad6e3ebf
ligovean <ligomail@rambler.ru>
2019-06-01 01:03:52 +0300

    Задание для собеседования !!!Корректно работает для любых размерностей массива добавлены "Бантики" для красивого вывода в консоль

--- src/Array.java
+++ src/Array.java
@@ -5,20 +5,21 @@ public class Array{
     public Array(int n) {
         this.n = n;
         this.arr= new int[n][n];
-        fillLine2(n,1,new int[]{0,0});
+        fillLine2(1,new int[]{0,0});
     }
 
     public void printArr(){
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(String.format("%02d", arr[i][j])+" ");
+                System.out.print(String.format("%0"+ ((Math.pow(n,2)+"").length()-2) + "d", arr[i][j])+" ");
             }
             System.out.printf("\n");
         }
     }
 
     //РЕКУРСИВНЫЙ МЕТОД ЗАПОЛНЕНИЯ СПИРАЛИ
-    public int[] fillLine2(int demn,int loop,int[] res){
+    public int[] fillLine2(int loop,int[] res){
         int yLine = res[0];
         int xLine = res[1];
         int lastN;
@@ -28,18 +29,20 @@ public class Array{
             lastN = 1;
 
         //Заполнение последнего элемента
-        if (lastN>=demn*demn-1)
-        {   if (demn%2!=0)
-            {arr[yLine][xLine] = lastN;
-            res[0]=yLine;
-            res[1]=xLine;}
+        if (lastN>=n*n-1)
+
+        {   res[0]=yLine;
+
+            if (n%2!=0)
+            {   arr[yLine][xLine] = lastN;
+                res[1]=xLine;}
             else
-            {res[0]=yLine;
-            res[1]=xLine-1;}
-            return res;}
+               res[1]=xLine-1;
+            return res;
+        }
 
         for (int tern=1;tern<5;tern++){
-            for (int i=0; i<demn-((loop-1)*2+1); i++){
+            for (int i=0; i<n-((loop-1)*2+1); i++){
                 switch (tern) {
                     case 1:
                         yLine = loop-1;
@@ -48,16 +51,16 @@ public class Array{
                         break;
                     case 2:
                         yLine = i+loop-1;
-                        xLine = demn-loop;
+                        xLine = n-loop;
                         arr[yLine][xLine] = lastN;//[Y][X]
                         break;
                     case 3:
-                        yLine = demn-loop;
-                        xLine = demn-loop-i;
+                        yLine = n-loop;
+                        xLine = n-loop-i;
                         arr[yLine][xLine] = lastN;//[Y][X]
                         break;
                     case 4:
-                        yLine = demn-loop-i;
+                        yLine = n-loop-i;
                         xLine = loop-1;
                         arr[yLine][xLine] = lastN;//[Y][X]
                         break;
@@ -67,7 +70,7 @@ public class Array{
         }
         res[0]=yLine;
         res[1]=xLine+1;
-        return fillLine2(demn,loop+1,res);
+        return fillLine2(loop+1,res);
     }
 
 }
